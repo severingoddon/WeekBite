2ba671013c0cc777fc68b6325e1bd4c150e51c45
sgoddon@swisslex.ch <sgoddon@swisslex.ch>
2026-02-19 16:53:58 +0100

    only persist db

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
-DATABASE_URL = "sqlite:///./cooking.db"
+DATABASE_URL = "sqlite:///./data/cooking.db"
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
